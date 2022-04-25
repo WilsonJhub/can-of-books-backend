4d59d49a2d86c159ed159878261131f2ed5232c1
Jason Wilson <jasonstimphil@gmail.com>
2022-04-25 17:27:29 -0400

    Forgot to create a separate branch. used schema to create book model. connected mongoose. reveived the terminal message of =mongoose is connected-

---
+++ models/books.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// require mongoose.
+const mongoose = require('mongoose');
+
+// extracting the schema property from the mongoose object
+const { Schema } = mongoose;
+
+// create a book schema, define how our book objects will be structured.
+const bookSchema = new Schema ({
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  status: {type: String, required: true}
+});
+
+
+// Define our model
+// Give it(?) funtionality and the predefined schema to our data.
+// Params: A string and a schema
+const bookModel = mongoose.model('Book', bookSchema);
+
+
+module.exports = bookModel
--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 'use strict';
 
+// Proof of Life
 console.log('This is your Can of Books Server')
 
+
+// 'require' for servers
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+// bring in mongoose
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL);
+
+
+// Bring in a schema to interact with that model
+const Book = require('./models/books');
+
+
+// add validation to confirm we are wired up to our mongo DB
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+
+// implement express
 const app = express();
 app.use(cors());
 
